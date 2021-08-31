369cbeadd237efc557a89099c34c0843a751cad2
shreyassanghvi <shreyas.sanghvi11@gmail.com>
2021-08-31 16:41:44 +0530

    setState not working in App.js

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,15 +1,34 @@
-
-import Card from "../components/Card";
-import NavBar from "../components/NavBar";
 import "tachyons"
+import {Component} from "react";
+import NavBar from "../components/NavBar";
+import Card from "../components/Card";
 
-function App() {
-    return (
-        <div className={"tc ma7"}>
-            <NavBar />
-            <Card Text="Chuck Norris Me"/>
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            joke: "Let's Get Going"
+        }
+    }
+    componentWillUnmount() {
+        this.setState({joke:""})
+    }
+    onRefresh = async function () {
+        const resp = await fetch("https://api.chucknorris.io/jokes/random")
+        const data = await resp.json();
+        this.setState({joke: data["value"]});
+    }
+
+    render() {
+        return (
+            <div className={"tc ma7"}>
+                <NavBar/>
+                <div>
+                    <Card Joke ={this.state.joke}/>
+                </div>
+                <button onClick={this.onRefresh} className={"white bg-black dib br3 pa3 grow ma4 f4"}>Let's Chuck it</button>
+            </div>
+        );
+    }
 }
-
 export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,16 @@
 import React from "react";
 
-const Card =  function ({Text}) {
+const Card = function ({Joke}) {
     try {
+
         return (
             <div className={"tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5"}>
-                <p>Lorem Epsum</p>
-                <div>
-                    <button>{Text}</button>
-                </div>
+                <p>{Joke}</p>
             </div>
         );
     } catch (e) {
         console.log(e);
     }
-
 };
 export default Card
 ;
